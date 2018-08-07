528c6cda70b49ca5a1daecb957a60e9f6c95cb79
yanmingjun <q195945056@qq.com>
2018-08-07 13:37:06 +0800

    Initial Commit

---
+++ PeachJapanese/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PeachJapanese
+//
+//  Created by yanmingjun on 2018/8/7.
+//  Copyright © 2018年 YouLoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PeachJapanese/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  PeachJapanese
+//
+//  Created by yanmingjun on 2018/8/7.
+//  Copyright © 2018年 YouLoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
